49c48a41ef27bdc970247100ad8ac25a82c0c3f7
Yasmim-CR <yas.castr0.r@gmail.com>
2025-02-24 10:17:04 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,2 @@
-﻿using System.Diagnostics;
-
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("digite um numero");
-double n1 = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("digite um outro numero");
-double n2 = Convert.ToDouble(Console.ReadLine());
-
-double total;
-total = n1 + n2;
-
-Console.WriteLine("total é: " + total);
